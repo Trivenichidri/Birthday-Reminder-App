8eb66786649a201a939ef26c8a465d35ef2dabde
Triveni <99028252+Trivenichidri@users.noreply.github.com>
2022-04-25 20:38:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+//import logo from "./logo.svg";
+import { useState } from "react";
+import data from "./data";
+import "./App.css";
+import List from "./List";
+
+function App() {
+  const [people, setPeople] = useState(data);
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people}></List>
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    name: "Triveni",
+    age: "23 years",
+    image: "https://randomuser.me/api/portraits/women/86.jpg",
+  },
+  {
+    id: 2,
+    name: "shourya",
+    age: "27 years",
+    image: "https://randomuser.me/api/portraits/men/76.jpg",
+  },
+  {
+    id: 3,
+    name: "rachit",
+    age: "26 years",
+    image: "https://randomuser.me/api/portraits/men/96.jpg",
+  },
+  {
+    id: 4,
+    name: "soumya",
+    age: "22 years",
+    image: "https://randomuser.me/api/portraits/women/33.jpg",
+  },
+];
